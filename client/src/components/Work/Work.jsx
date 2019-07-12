1f570f49278f981bb199e24efb10f3b57b63dcd7
Ryan Cowie <ryancowie@Ryans-Pro.hsd1.ca.comcast.net>
2019-07-12 02:22:55 -0700

    Adds animation to work section

--- client/src/components/Work/Work.jsx
+++ client/src/components/Work/Work.jsx
@@ -32,15 +32,32 @@ class Work extends React.Component {
       <div>
         <div className="Work-Container">
           <div className="Work-Info-Container">
+          <Fade
+            up
+            duration={3100}
+            when={pagePosition > 860}
+            distance={'400px'}
+          >
             <h2 className="Work-Title">Numerous Happy Customers</h2>
             <h3 className="Work-Sub-Title">And Counting...</h3>
+          </Fade>
+          <Fade
+            down
+            duration={3100}
+            when={pagePosition > 860}
+            distance={'400px'}
+          >
             <div className="Star-Container">
               <div>{star}</div><div>{star}</div><div>{star}</div><div>{star}</div><div>{star}</div>
             </div>
+          </Fade>
           </div>
           <div className="Carousel-Container">
             <WorkCarousel />
           </div>
+        </div>
+        <div className="Contact-Container">
+
         </div>
       </div>
     )
